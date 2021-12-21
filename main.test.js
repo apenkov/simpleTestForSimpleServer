2a58ef931b91949e87b859d3735b0607e6a0187f
apenkov <organaizer1212@gmail.com>
2021-12-21 19:57:08 +0300

    make test more simple

--- main.test.js
+++ main.test.js
@@ -1,15 +1,17 @@
-const http = require('http');
+// const http = require('http');
+//
+// const url = 'http://localhost:3000/'
+//
+// http.get(url, res => {
+//   console.log(res.statusCode);
+//   let data = ''
+//   res.on('data', chunk => {
+//     data +=chunk
+//   })
+//
+//   res.on('end', () => {
+//     console.log(data)
+//   })
+// })
 
-const url = 'http://localhost:3000/'
-
-http.get(url, res => {
-  console.log(res.statusCode);
-  let data = ''
-  res.on('data', chunk => {
-    data +=chunk
-  })
-
-  res.on('end', () => {
-    console.log(data)
-  })
-})
+console.log('hello')
